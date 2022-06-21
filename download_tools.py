11c50e057517bb50fff766b4de4a34a0c1081e94
CelebV-HQ <102649654+CelebV-HQ@users.noreply.github.com>
2022-06-21 13:20:28 +0800

    Update download_tools.py
    add comments

--- download_tools.py
+++ download_tools.py
@@ -1,22 +1,27 @@
 import cv2
 import os
-import numpy as np
 import json
 
-def download(video_id, video_folder, proxy=None):
+
+def download(ytb_id, save_folder, proxy=None):
+    """
+    ytb_id: youtube_id
+    save_folder: save video folder
+    proxy: proxy url, defalut None
+    """
     if proxy is not None:
         proxy_cmd = "--proxy {}".format(proxy)
     else:
         proxy_cmd = ""
-    video_path = os.path.join(video_folder, video_id + ".mp4")
+    video_path = os.path.join(save_folder, ytb_id + ".mp4")
     if not os.path.exists(video_path):
         down_video = " ".join([
-            "youtube-dl", 
-             proxy_cmd,
-            '-f', "'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'", 
+            "youtube-dl",
+            proxy_cmd,
+            '-f', "'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'",
             '--skip-unavailable-fragments',
             '--merge-output-format', 'mp4',
-            "https://www.youtube.com/watch?v=" + video_id, "--output",
+            "https://www.youtube.com/watch?v=" + ytb_id, "--output",
             video_path, "--external-downloader", "aria2c",
             "--external-downloader-args", '"-x 16 -k 1M"'
         ])
@@ -26,9 +31,13 @@ def download(video_id, video_folder, proxy=None):
     return video_path
 
 
-def process_ffmpeg(path, save_path, out_vid_name, box, time):
+def process_ffmpeg(raw_vid_path, save_folder, save_vid_name,
+                   bbox, time):
     """
-    box: top, bottom, left, right, normalized to 0~1
+    raw_vid_path: 
+    save_folder: 
+    save_vid_name: 
+    bbox: format: top, bottom, left, right. the values are normalized to 0~1
     time: begin_sec, end_sec
     """
 
@@ -40,14 +49,14 @@ def process_ffmpeg(path, save_path, out_vid_name, box, time):
         return "{:02d}:{:02d}:{:02d}.{:02d}".format(int(hrs), int(min),
                                                     int(sec), int(end))
 
-    def expand(box, ratio):
-        top, bottom = max(box[0] - ratio, 0), min(box[1] + ratio, 1)
-        left, right = max(box[2] - ratio, 0), min(box[3] + ratio, 1)
+    def expand(bbox, ratio):
+        top, bottom = max(bbox[0] - ratio, 0), min(bbox[1] + ratio, 1)
+        left, right = max(bbox[2] - ratio, 0), min(bbox[3] + ratio, 1)
 
         return top, bottom, left, right
 
-    def to_square(box):
-        top, bottom, leftx, right = box
+    def to_square(bbox):
+        top, bottom, leftx, right = bbox
         h = bottom - top
         w = right - leftx
         c = min(h, w) // 2
@@ -58,36 +67,37 @@ def process_ffmpeg(path, save_path, out_vid_name, box, time):
         leftx, right = c_w - c, c_w + c
         return top, bottom, leftx, right
 
-    def denorm(box, height, width):
+    def denorm(bbox, height, width):
         top, bottom, left, right = \
-                round(box[0] * height), \
-                round(box[1] * height), \
-                round(box[2] * width), \
-                round(box[3] * width)
+            round(bbox[0] * height), \
+            round(bbox[1] * height), \
+            round(bbox[2] * width), \
+            round(bbox[3] * width)
 
         return top, bottom, left, right
 
-    out_name = os.path.join(save_path, out_vid_name)
+    out_path = os.path.join(save_folder, save_vid_name)
 
-    cap = cv2.VideoCapture(path)
+    cap = cv2.VideoCapture(raw_vid_path)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     top, bottom, left, right = to_square(
-        denorm(expand(box, 0.02), height, width))
+        denorm(expand(bbox, 0.02), height, width))
     start_sec, end_sec = time
 
-    cmd = f"ffmpeg -i {path} -vf crop=w={right-left}:h={bottom-top}:x={left}:y={top} -ss {secs_to_timestr(start_sec)} -to {secs_to_timestr(end_sec)} -loglevel error {out_name}"
+    cmd = f"ffmpeg -i {raw_vid_path} -vf crop=w={right-left}:h={bottom-top}:x={left}:y={top} -ss {secs_to_timestr(start_sec)} -to {secs_to_timestr(end_sec)} -loglevel error {out_path}"
     os.system(cmd)
-    return out_name
+    return out_path
+
 
 def load_data(file_path):
     with open(file_path) as f:
         data_dict = json.load(f)
 
-    for key,val in data_dict['clips'].items():
+    for key, val in data_dict['clips'].items():
         save_name = key+".mp4"
         ytb_id = val['ytb_id']
-        time = val['duration']['start_sec'],val['duration']['end_sec']
+        time = val['duration']['start_sec'], val['duration']['end_sec']
 
         print(ytb_id)
         bbox = [val['bbox']['top'], val['bbox']['bottom'], val['bbox']['left'], val['bbox']['right']]
@@ -95,15 +105,16 @@ def load_data(file_path):
 
 
 if __name__ == '__main__':
-    file_path = 'celebvhq_35666.json'
-    raw_root = './video/raw/'
-    processed_root = './video/processed/'
+    json_path = 'celebvhq_35666.json' # json file path 
 
-    proxy = "http://172.16.1.135:3128/"
+    raw_vid_root = './downloaded_celebvhq/raw/' # download raw video path
+    processed_vid_root = './downloaded_celebvhq/processed/' # processed video path
 
-    os.makedirs(raw_root, exist_ok=True)
-    os.makedirs(processed_root, exist_ok=True)
+    proxy = "http://172.16.1.135:3128/" # proxy url example, set to None if not use 
 
-    for vid_id, out_vid_name, time, box in load_data(file_path):
-        raw_vid_path = download(vid_id, raw_root, proxy)
-        process_ffmpeg(raw_vid_path, processed_root, out_vid_name, box, time)
+    os.makedirs(raw_vid_root, exist_ok=True)
+    os.makedirs(processed_vid_root, exist_ok=True)
+
+    for vid_id, save_vid_name, time, bbox in load_data(json_path):
+        raw_vid_path = download(vid_id, raw_vid_root, proxy)
+        process_ffmpeg(raw_vid_path, processed_vid_root, save_vid_name, bbox, time)
